9becca6a3f1ddfcef3c33f9b91262178e55f3aa1
ZG1203 <z0395621@gmail.com>
2025-11-23 15:08:01 +0000

    updated repo detail page

--- src/pages/ProjectDetailPage.jsx
+++ src/pages/ProjectDetailPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const ProjectDetailPage = ({ repo }) => {
+   console.log('Full repo:', repo);
+
   return (
     <div>
-      <h2>Welcome to the Project Detail Page</h2>
-      <p>This is the project detail page.</p>
+      <h2>Project Detail</h2>
+      <h3>Project Name: {repo.name}</h3>
+      <p>Date Created: {new Date(repo.created_at).toLocaleDateString()}</p>
+      <p>Language: {repo.language}</p>
+      <a href={repo.html_url} target="_blank" >Link to Github Repo</a>
     </div>
   );
 };
